d3ffb338a6b342da58f94b4fe98452e60b04541e
Baptiste <baptistev59@free.fr>
2025-09-24 10:58:27 +0100

    mise à jour de la database en ligne

--- Config/config.php
+++ Config/config.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'host' => 'localhost',
-    'db_name' => 'doctolight',
-    'username' => 'root',
-    'password' => '',
+    'host' => 'mysql-baptistev59.alwaysdata.net',
+    'db_name' => 'baptistev59_doctolight',
+    'username' => '428185',
+    'password' => 'Teqapexa59!',
     'role_hierarchy' => ['ADMIN', 'SECRETAIRE', 'MEDECIN', 'PATIENT']
 ];
