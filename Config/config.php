0658c4c117d6db3c208986bec84ea7f3dda803dd
Baptiste <baptistev59@free.fr>
2025-09-24 14:33:43 +0100

    mise en place du .gitignore et modif de la config

--- Config/config.php
+++ Config/config.php
@@ -1,10 +1,20 @@
 <?php
 $envFile = __DIR__ . '/.env';
+$envFileLocal = __DIR__ . '/.env.local';
 $env = [];
 
 // Lecture du fichier .env
 if (file_exists($envFile)) {
     foreach (file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) as $line) {
+        // Ignorer les commentaires commençant par #
+        if (strpos(trim($line), '#') === 0) continue;
+
+        // Découpage clé=valeur
+        [$key, $value] = explode('=', $line, 2);
+        $env[trim($key)] = trim($value);
+    }
+} elseif (file_exists($envFileLocal)) {
+    foreach (file($envFileLocal, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) as $line) {
         // Ignorer les commentaires commençant par #
         if (strpos(trim($line), '#') === 0) continue;
 
